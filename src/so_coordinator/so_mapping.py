df77f95ecf5f53de5814d9bf268a578c2ae451ea
Tanja Kaiser <tanjakkaiser@gmail.com>
2017-03-29 12:16:13 +0200

    specified complete so mapping

--- src/so_coordinator/so_mapping.py
+++ src/so_coordinator/so_mapping.py
@@ -2,31 +2,113 @@
 Created on 24.03.2017
 
 @author: kaiser
+
+Module containing mapping from strings to classes
 """
 
-from so_data.chemotaxis import ChemotaxisBalch
-from behaviour_components.activators import LinearActivator, BooleanActivator
+from behaviour_components.activators import MultiSensorCondition, \
+    PublisherCondition, BooleanActivator, ThresholdActivator, GreedyActivator, \
+    LinearActivator
+from behaviour_components.conditions import Disjunction, Conjunction, Negation
+from behaviour_components.goals import GoalBase, OfflineGoal, PublisherGoal
+from behaviour_components.sensors import PassThroughTopicSensor, \
+    SimpleTopicSensor, DynamicSensor
+
+from rhbp_selforga.behaviours import MoveBehaviour, TurnBehaviour, \
+    DecisionBehaviour, DecisionStateBehaviour, SetStateBehaviour
+from rhbp_selforga.conditions import ChangeFloatCondition, \
+    ChangeStringCondition, BoolTCondition, BoolFCondition, VectorBoolCondition,\
+    GoalBoolCondition, MaxDistCondition, VectorDistCondition
 from rhbp_selforga.gradientsensor import GradientSensor, SENSOR
-from rhbp_selforga.conditions import VectorBoolCondition, GoalBoolCondition, \
-    VectorDistCondition
-from rhbp_selforga.behaviours import MoveBehaviour
-from behaviour_components.goals import GoalBase
-from behaviour_components.conditions import Negation, Conjunction
-from so_data.sobuffer import SoBuffer
+
+from so_data.chemotaxis import ChemotaxisGe, ChemotaxisBalch, \
+    CollisionAvoidance, FollowAll, AvoidAll, FollowMax, FollowMin, \
+    FollowMinReach, FollowMaxReach
+from so_data.decisions import MorphogenesisBarycenter, GossipMax, Quorum
+from so_data.flocking import Flocking
+from so_data.flockingrey import FlockingRey
+from so_data.foraging import ForagingDecision, DepositPheromones, \
+    Exploitation, Exploration, STATE
+from so_data.repulsion import RepulsionFernandez, RepulsionGradient
+from so_data.sobuffer import SoBuffer, AGGREGATION
+from so_data.supplements import DepositPheromonesMin, DepositPheromonesRandom
 
 
 SO_MAPPING = {
+
+    # RHBP COMPONENTS
+    # Activators
+    'MultiSensorCondition': MultiSensorCondition,
+    'PublisherCondition': PublisherCondition,
+    'BooleanActivator': BooleanActivator,
+    'ThresholdActivator': ThresholdActivator,
+    'GreedyActivator': GreedyActivator,
+    'LinearActivator': LinearActivator,
+    # Conditions
+    'Disjunction': Disjunction,
+    'Conjunction': Conjunction,
     'Negation': Negation,
+    # Goals
     'GoalBase': GoalBase,
+    'OfflineGoal': OfflineGoal,
+    'PublisherGoal': PublisherGoal,
+    # Sensors
+    'PassThroughTopicSensor': PassThroughTopicSensor,
+    'SimpleTopicSensor': SimpleTopicSensor,
+    'DynamicSensor': DynamicSensor,
+
+    # RHBP_SELFORGA COMPONENTS
+    # Behaviours
     'MoveBehaviour': MoveBehaviour,
+    'TurnBehaviour': TurnBehaviour,
+    'DecisionBehaviour': DecisionBehaviour,
+    'DecisionStateBehaviour': DecisionStateBehaviour,
+    'SetStateBehaviour': SetStateBehaviour,
+    # Conditions
+    'ChangeFloatCondition': ChangeFloatCondition,
+    'ChangeStringCondition': ChangeStringCondition,
+    'BoolTCondition': BoolTCondition,
+    'BoolFCondition': BoolFCondition,
     'VectorBoolCondition': VectorBoolCondition,
+    'MaxDistCondition': MaxDistCondition,
     'GoalBoolCondition': GoalBoolCondition,
     'VectorDistCondition': VectorDistCondition,
+    # Gradient Sensor
     'GradientSensor': GradientSensor,
     'SENSOR': SENSOR,
-    'LinearActivator': LinearActivator,
-    'BooleanActivator': BooleanActivator,
+
+    # SO_DATA COMPONENTS
+    # Chemotaxis
+    'ChemotaxisGe': ChemotaxisGe,
     'ChemotaxisBalch': ChemotaxisBalch,
+    'CollisionAvoidance': CollisionAvoidance,
+    'FollowAll': FollowAll,
+    'AvoidAll': AvoidAll,
+    'FollowMax': FollowMax,
+    'FollowMin': FollowMin,
+    'FollowMinReach': FollowMinReach,
+    'FollowMaxReach': FollowMaxReach,
+    # Decisions
+    'MorphogenesisBarycenter': MorphogenesisBarycenter,
+    'GossipMax': GossipMax,
+    'Quorum': Quorum,
+    # Flocking
+    'Flocking': Flocking,
+    # FlockingRey
+    'FlockingRey': FlockingRey,
+    # Foraging
+    'ForagingDecision': ForagingDecision,
+    'DepositPheromones': DepositPheromones,
+    'Exploitation': Exploitation,
+    'Exploration': Exploration,
+    'STATE': STATE,
+    # Repulsion
+    'RepulsionFernandez': RepulsionFernandez,
+    'RepulsionGradient': RepulsionGradient,
+    # Sobuffer
     'SoBuffer': SoBuffer,
-    'Conjunction': Conjunction
+    'AGGREGATION': AGGREGATION,
+    # supplements
+    'DepositPheromonesMin': DepositPheromonesMin,
+    'DepositPheromonesRandom': DepositPheromonesRandom
 }
