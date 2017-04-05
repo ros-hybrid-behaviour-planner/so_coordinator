f3ac7c5c189cbc1729dcc02dbf9633820ed43a18
Tanja Kaiser <tanjakkaiser@gmail.com>
2017-04-05 09:49:16 +0200

    comments soc.py

--- src/so_coordinator/soc.py
+++ src/so_coordinator/soc.py
@@ -19,7 +19,6 @@ class SoCoordinator(NetworkBehavior):
     problems
     """
 
-    # requires execution steps = True is super important!
     def __init__(self, effects, so_goal, id=1,
                  expert_knowledge='so_knowledge.yaml',
                  components_class=SOComponents, name='SoCoordinator',
@@ -27,27 +26,32 @@ class SoCoordinator(NetworkBehavior):
                  pose_frame='robot', motion_topic='', **kwargs):
         """
         initialization
-        :param effect:
-        :param components_class:
-        :param id:
-        :param so_goal:
-        :param expert_knowledge:
-        :param name:
+        :param effects: effects of SoCoordinator (on higher level)
+        :param so_goal: self-organization goal used in coordination mechanism
+                        selection
+        :param id: id of the agent
+        :param expert_knowledge: yaml file containing mapping between so goal
+                                 and setup of RHBP components
+        :param components_class: class containing factory to create RHBP
+                                 components
+        :param name: unique name of the object
+        :param mapping: dictionary mapping strings to classes
         :param requires_execution_steps: whether the execution steps should be
                                          caused from the parent manager or not.
-        :param kwargs:
+        :param pose_frame: header frame ID indicating robot pose gradients
+        :param motion_topic: motion topic of the agent
+        :param kwargs: keyword arguments
         """
 
-        self.name = name
-
-        self.manager_name = name + NetworkBehavior.MANAGER_POSTFIX
-
-        super(SoCoordinator, self).__init__(effects=effects, name=self.name,
+        # init Network Behaviour
+        super(SoCoordinator, self).__init__(effects=effects, name=name,
                                             requires_execution_steps=
                                             requires_execution_steps,
                                             **kwargs)
 
-        # create SO components
+        # Coordination Mechanism Selection
+
+        # create SO components based on expert knowledge
         components = create_from_yaml(os.path.join(os.path.dirname(__file__),
                                                    expert_knowledge),
                                       id,
@@ -57,4 +61,3 @@ class SoCoordinator(NetworkBehavior):
                                       mapping=mapping,
                                       components_class=components_class,
                                       pose_frame=pose_frame)
-
